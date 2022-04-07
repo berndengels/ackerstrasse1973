b1b4f53ef5fad31278ee4b9b913b643ae6b68ae0
berndengels <99887389+bta-engels@users.noreply.github.com>
2022-04-07 21:56:51 +0200

    poster

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php require_once 'inc/header.php' ?>
 
 <div class="row">
-    <video class="col-sm-12 col-md-auto" controls>
+    <video class="col-sm-12 col-md-auto" poster="/media/ackerstrasse1973.jpg" controls>
         <source src="/media/ackerstrasse1973.mp4" type="video/mp4">
     </video>
 </div>
